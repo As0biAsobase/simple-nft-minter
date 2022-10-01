baa138695bd38b07780152bca651e6412e097a3a
Gleb Rysev <glebrysev@gmail.com>
2022-10-01 13:35:17 +0100

    Moved stuff to functions

--- core.py
+++ core.py
@@ -15,9 +15,6 @@ def time_until(ts):
 def fetch_addresses():
     return json.load(open('keys_adresses.json'))["data"]
 
-def gas_estimate(w3, multiplier):
-    return int(w3.eth.gas_price / 10**9) * multiplier
-
 def mint(signed_tx):
     try:
         sent_tx = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
@@ -28,14 +25,7 @@ def mint(signed_tx):
 def test(contract, key, address):
     print(contract, key, address)
 
-
-def main():
-    contract_address = '0x3DD5e0f0659cA8b52925E504FE9f0250bFe68301'
-    print(f'Atemptimg a mint on {contract_address}')
-
-    target_contract = w3.eth.contract(address=contract_address, abi=fetch_abi(contract_address))
-    print(f'Contract ABI fetched, determining the start time...')
-    
+def catch_initialization(target_contract):
     event_filter = target_contract.events.Initialized.createFilter(fromBlock='latest')
     start_time = 0
     while True:
@@ -49,18 +39,14 @@ def main():
         else: 
             time.sleep(0.2)
 
-    time_left = time_until(start_time)
-    print(f'{time_left} seconds left')
+    return start_time
 
-    keys_addresses = fetch_addresses()
-    print(f'Got {len(keys_addresses)} wallets, building the transactions array...')
-
+def sign_transactions(keys_addresses, target_contract):
     signed_transactions = []
     for pair in keys_addresses:
         mint_txn = target_contract.functions.publicSaleMint(
             1
         ).buildTransaction({
-            # 'value' : 2*10**18,
             'gas': 300000,
             'maxFeePerGas': Web3.toWei('300', 'gwei'),
             'maxPriorityFeePerGas': Web3.toWei('50', 'gwei'),
@@ -69,6 +55,25 @@ def main():
 
         signed_tx = w3.eth.account.sign_transaction(mint_txn, private_key=pair["key"])
         signed_transactions.append(signed_tx)
+
+    return signed_transactions
+
+def main():
+    contract_address = '0x3DD5e0f0659cA8b52925E504FE9f0250bFe68301'
+    print(f'Atemptimg a mint on {contract_address}')
+
+    target_contract = w3.eth.contract(address=contract_address, abi=fetch_abi(contract_address))
+    print(f'Contract ABI fetched, determining the start time...')
+    
+    start_time = catch_initialization(target_contract)
+
+    time_left = time_until(start_time)
+    print(f'{time_left} seconds left')
+
+    keys_addresses = fetch_addresses()
+    print(f'Got {len(keys_addresses)} wallets, building the transactions array...')
+
+    signed_transactions = sign_transactions(keys_addresses)
 
     while time.time() < start_time:
         time.sleep(0.001)
@@ -84,7 +89,7 @@ def main():
 
 
 if __name__ == "__main__":
-    global w3 
+    global w3
     w3 = Web3(Web3.HTTPProvider('https://api.avax.network/ext/bc/C/rpc'))
     print(f"Welcome, web3 connection - {w3.isConnected()}")
 
